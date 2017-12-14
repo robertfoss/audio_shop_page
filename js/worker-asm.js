8194c5fb250835cc9802d644eb0f2254cd56d15e
Robert Foss <robert.foss@collabora.com>
2017-12-14 22:25:33 +0100

    js: Add ffmpeg worker

---
+++ js/worker-asm.js
@@ -0,0 +1,64 @@
+importScripts('ffmpeg-worker.js');
+
+var now = Date.now;
+
+function print(text) {
+  postMessage({
+    'type' : 'stdout',
+    'data' : text
+  });
+}
+function printErr(text) {
+  postMessage({
+    'type' : 'stderr',
+    'data' : text
+  });
+}
+
+onmessage = function(event) {
+
+  var message = event.data;
+
+  if (message.type === "command") {
+debugger;
+    var Module = {
+      print: print,
+      printErr: printErr,
+      files: message.files || [],
+      arguments: message.arguments || [],
+      TOTAL_MEMORY: 268435456
+      // Can play around with this option - must be a power of 2
+      // TOTAL_MEMORY: 268435456
+    };
+
+    postMessage({
+      'type' : 'start',
+      'data' : Module.arguments.join(" ")
+    });
+
+    postMessage({
+      'type' : 'stdout',
+      'data' : 'ffmpeg ' +
+                Module.arguments.join(" ")
+    });
+
+    var time = now();
+    var result = ffmpeg_run(Module);
+
+    var totalTime = now() - time;
+    postMessage({
+      'type' : 'stdout',
+      'data' : 'Finished processing (took ' + totalTime + 'ms)'
+    });
+
+    postMessage({
+      'type' : 'done',
+      'data' : result,
+      'time' : totalTime
+    });
+  }
+};
+
+postMessage({
+  'type' : 'ready'
+});
